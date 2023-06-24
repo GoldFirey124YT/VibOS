68c7f5e233bc0f4d39a23fc0e2a88c9240b1a0d0
GoldFirey <81136576+GoldFirey124YT@users.noreply.github.com>
2023-06-24 19:06:56 +1000

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,21 +1,54 @@
+import tkinter as tk
 import os
-import time
 
-print("Welcome To VibOS Beta :\\")
-print("Install at your own risk; This isnt quite finish but please let me know :)")
-input('Start installing VibOS? ')
-print('Installing VibOS..')
-print('Please wait for a little while, while we prepare.')
-print('If you see anything about a APT lock, please reboot your computer.')
+def setup():
+  os.system('sudo apt-get update')
+  os.system('sudo apt -y install plymouth-themes')
+  os.system('sudo apt -y install python3-tk')
+  os.system('sudo apt -y install git')
+  os.system('python3 Start2.py')
 
-time.sleep(3)
+def show_tip():
+  root.tk.messagebox.showinfo('VibOS Tip', 'VibOS makes your system lightweight for old and new hardware.')
 
-os.system('sudo apt-get update') # Always run this first, can fix broken installs.
+root = tk.Tk()
+root.title('VibOS Installer')
+root.geometry('300x200')
 
-os.system('sudo apt -y install plymouth-themes')
-os.system('sudo apt -y install python3-tk')
-os.system('sudo apt -y install git')
+label = tk.Label(root, text='Welcome to the VibOS installer!')
+label.pack()
 
-print('\nFinishing up.')
+button = tk.Button(root, text='Start Setup', command=setup)
+button.pack()
 
-os.system('python3 Start2.py')
+tip_button = tk.Button(root, text='Show Tip', command=show_tip)
+tip_button.pack()
+
+root.mainloop()
+
+def Clean():
+  os.system('sudo cp $PWD/Assets/CleanGold.png /usr/share/backgrounds')
+
+def DClean():
+  os.system('sudo cp $PWD/Assets/CleanGoldDark.png /usr/share/backgrounds')
+
+def Firey():
+  os.system('sudo cp $PWD/Assets/GoldFirey.png /usr/share/backgrounds')
+
+def DFirey():
+  os.system('sudo cp $PWD/Assets/GoldFireyDark.png /usr/share/backgrounds')
+
+def Horri():
+  os.system('sudo cp $PWD/Assets/GoldHorizon.png /usr/share/backgrounds')
+
+def DHorri():
+  os.system('sudo cp $PWD/Assets/GoldHorizonDark.png /usr/share/backgrounds')
+
+def copy_osdata():
+  with open('OSDATA.txt', 'r') as f:
+    data = f.read()
+  os.system(f'sudo cp {data} /etc/os-release')
+
+if __name__ == '__main__':
+  setup()
+  copy_osdata()
